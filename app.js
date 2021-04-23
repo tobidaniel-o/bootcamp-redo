4c5b4b2fec317197f093c31087af966557252945
Tobi <tobi.daniel2018@gmail.com>
2021-04-23 10:37:55 -0400

    updated

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ const ulElement = document.querySelector('.movieGenres')
 genres.forEach((genre) => {
   const newListItem = document.createElement('li')
   newListItem.textContent = genre
-  ulElement.appendChild(newListItem)
+  ulElement.append(newListItem)
 })
+
+axios({
+  method: "GET",
+  url: "http://api.site.com/api",
+  responseType: "json",
+  params: {
+    param1: "value",
+  },
+}).then(function (res) {
+  console.log(res);
+});
+
